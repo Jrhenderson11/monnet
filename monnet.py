d2585b447219057e8997212fe018ea69195742c7
hendo <16228548+Jrhenderson11@users.noreply.github.com>
2022-09-19 11:02:01 +0100

    Added SYN detection

--- monnet.py
+++ monnet.py
@@ -8,6 +8,66 @@ import pyshark
 import argparse
 import netifaces as ni
 
+class CustomHChart(HChart):
+    """Horizontal chart. Values must be between 0 and 100 and can be float.
+    """
+    def __init__(self, val=100, *args, **kw):
+        super(CustomHChart, self).__init__(**kw)
+        self.value = val
+        self.datapoints = deque(maxlen=500)
+        
+
+    def _display(self, tbox, parent):
+        tbox = self._draw_borders_and_title(tbox)
+        print(tbox.t.color(self.color))
+
+        flags = {}
+        for dx in range(tbox.w):
+            dp_index = -tbox.w + dx
+            try:
+                dp = self.datapoints[dp_index]
+                    
+                if type(dp) in [str]:
+                    flags[dp_index] = [x for x in dp if x.isdigit() ==False]
+            except (IndexError,KeyError):
+                pass
+
+        for dy in range(tbox.h):
+            bar = ""
+            for dx in range(tbox.w):
+                dp_index = -tbox.w + dx
+                try:
+                    dp = self.datapoints[dp_index]
+                    if type(dp) in [str]:
+                        dp = int(''.join([x for x in dp if x.isdigit()==True]))
+
+                    q = (1 - dp / 100) * tbox.h
+                    if dy < int(q):
+                        bar += " "
+                    else:
+                        if flags[dp_index] != []:
+                            bar += flags[dp_index][0]
+                            if len(flags)>1:
+                                flags[dp_index] = flags[dp_index][1:]
+                            else:
+                                flags[dp_index] = []
+                        elif dy == int(q):
+                            index = int((int(q) - q) * 8 - 1)
+                            bar += vbar_elements[index]
+                        else:
+                            bar += vbar_elements[-1]
+                except (IndexError,KeyError):
+                    bar += " "
+
+            # assert len(bar) == tbox.w
+            print(tbox.t.move(tbox.x + dy, tbox.y) + bar)
+
+class DataContainer():
+
+    def __init__(self, count=0, flags=''):
+        self.count = count
+        self.flags = flags
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--interface', help='interface to listen to', default='any')
@@ -16,17 +76,18 @@ parser.add_argument('ports', help='ports to listen to, comma separated')
 if os.geteuid() != 0:
     exit("root privileges needed")
 
-
 args = parser.parse_args()
-
-
 
 iface = args.interface
 
-if iface == "any":
-    dsts = [ni.ifaddresses(i)[ni.AF_INET][0]['addr'] for i in ni.interfaces() if ni.AF_INET in ni.ifaddresses(i)]
-else:
-    dsts = [ni.ifaddresses(iface)[ni.AF_INET][0]['addr']]
+try:
+    if iface == "any":
+        dsts = [ni.ifaddresses(i)[ni.AF_INET][0]['addr'] for i in ni.interfaces() if ni.AF_INET in ni.ifaddresses(i)]
+    else:
+        dsts = [ni.ifaddresses(iface)[ni.AF_INET][0]['addr']]
+except pyshark.capture.live_capture.UnknownInterfaceException:
+    print(f"Error: interface {iface} unkown, exiting.")
+    exit()
 
 icmp = False
 
@@ -47,7 +108,7 @@ colour = 1
 for i,port in enumerate(ports):
     colour = colour+1 % 20
 
-    new = HSplit(HChart(title=f'{port}', border_color=7, color=colour))
+    new = HSplit(CustomHChart(title=f'{port}', border_color=7, color=colour))
     portcharts[port] = new.items[0]
     ui.items+=(new,)
 
@@ -61,20 +122,29 @@ try:
         packets = [pkt for pkt in capture._packets]
         capture.close()
 
-        counts = {p:0 for p in ports}
+        counts = {port:DataContainer() for p in ports}
         
         for packet in packets:
             if 'ip' in packet and packet['ip'].dst in dsts:
                 if 'tcp' in packet:
                     for port in ports:
                         if port == int(packet['tcp'].dstport):
-                            counts[port] += 1
+                            counts[port].count += 1
+                            if (packet['tcp'].flags_syn == '1' and 'S' not in counts[port].flags):
+                                counts[port].flags += 'S'
+                            if (packet['tcp'].flags_res == '1' and 'R' not in counts[port].flags):
+                                counts[port].flags += 'R'
             elif 'ICMP' in packet:
-                counts['icmp'] += 1
+                counts['icmp'].count += 1
 
         for port in ports:
-            portcharts[port].append((counts[port]*5))
+            count = (counts[port].count*5)
+            item = count
+            if counts[port].flags != '':
+                item = counts[port].flags + str(count)
 
+            portcharts[port].append(item)
+        
         ui.display()
 
 except KeyboardInterrupt:
